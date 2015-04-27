9db6f1acd85c4cfad6058a80a829463525316f41
Douglas Green <bluntsporks+git@gmail.com>
2015-04-27 14:26:02 -0400

    Adding capitalization

--- respell.go
+++ respell.go
@@ -45,18 +45,29 @@ func main() {
 		words := matchWords(line)
 		for _, word := range words {
 			upword := strings.ToUpper(word)
+			isUpper := word != strings.ToLower(word)
 			pron := cmuDict[upword]
 			if pron == "" {
 				fmt.Print(word)
 			} else {
 				phonemes := cmudict.GetPhonemes(pron, true)
 				fixed := fixPhonemes(phonemes)
-				for _, phoneme := range fixed {
+				for i, phoneme := range fixed {
 					bare := cmudict.StripAccent(phoneme)
 					spelling := spellings[bare]
 					output := phoneme
 					if spelling != "" {
 						output = spelling
+					}
+
+					// Capitalize the first letter of output if the original word was not
+					// lowercase.
+					if i == 0 && isUpper {
+						capd := strings.ToUpper(output[0:1])
+						if len(output) > 1 {
+							capd += output[1:]
+						}
+						output = capd
 					}
 					fmt.Print(output)
 				}
