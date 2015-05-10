93355b8ce5d11a5643b50393824be0a1ff2f9804
Douglas Green <bluntsporks+git@gmail.com>
2015-05-10 10:06:05 -0400

    Moving function

--- funcs.go
+++ funcs.go
@@ -3,11 +3,9 @@ package main
 
 import (
 	"bufio"
-	"github.com/BluntSporks/cmudict"
 	"log"
 	"os"
 	"path"
-	"regexp"
 	"strings"
 )
 
@@ -18,49 +16,6 @@ func defaultSpellPath() string {
 		log.Fatal("Set PHONUTIL_DATA variable to directory of spelling file")
 	}
 	return path.Join(dir, "spellings.csv")
-}
-
-// fixPhonemes fixes a pronunciation string with custom fixes designed for better phonetic spelling.
-func fixPhonemes(phonemes []string) []string {
-	n := len(phonemes)
-	newPhonemes := make([]string, 0, 2*n)
-	for i := 0; i < n; i++ {
-		phoneme := phonemes[i]
-		if len(phoneme) > 2 && phoneme[:2] == "ER" {
-			// Use ahx r instead of erx.
-			newPhonemes = append(newPhonemes, "AH"+string(phoneme[2]))
-			phoneme = "R"
-		} else if i < n-1 {
-			if phoneme == "HH" && phonemes[i+1] == "W" {
-				// Use wh instead of hw.
-				phoneme = "WH"
-				i++
-			}
-		}
-		newPhonemes = append(newPhonemes, phoneme)
-	}
-	n = len(newPhonemes)
-	out := make([]string, 0, 2*n)
-	for i, ph := range newPhonemes {
-		out = append(out, ph)
-		if i < n-1 {
-			// Use an apostrophe to split up ambiguous combinations of sounds.
-			split := false
-			if newPhonemes[i+1] == "HH" {
-				if ph == "D" || ph == "S" || ph == "T" || ph == "W" || ph == "Z" {
-					split = true
-				}
-			} else if ph == "N" && newPhonemes[i+1] == "G" {
-				split = true
-			} else if cmudict.IsVowel(ph) && cmudict.IsVowel(newPhonemes[i+1]) {
-				split = true
-			}
-			if split {
-				out = append(out, "'")
-			}
-		}
-	}
-	return out
 }
 
 // loadWordList loads a list of words in a file and returns it as an uppercased lookup list.
